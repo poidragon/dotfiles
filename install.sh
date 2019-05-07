9fb41041cfdedc26482be0c2a13971a0dcd05cef
Jeffrey <jeffrey@smallhd.com>
2019-05-07 09:45:51 -0400

    added install

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# This installation script should ensure all of the dotfiles from this 
+# directory are installed into the home directory, hopefully without
+# destroying any existing files.
+#
+# Currently all it does is blindly copy everything over
+cp ./.* ~/
